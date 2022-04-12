907fbb767c0a9539b8af1b1acfb2ef161662ec03
Matheus Reis <matheus.reis081@gmail.com>
2022-04-11 22:29:19 -0300

    Header da Home feito

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,10 +1,10 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
     flex:1;
 
     align-items:center;
-    justify-content:center;
     align-content:center;
     
     
@@ -13,11 +13,34 @@ export const Container = styled.View`
     background-color: ${({theme})=>theme.colors.background_primary};
 `;
 
+export const HeaderContent = styled.View`
 
 
-export const Title = styled.Text`
+align-content:center;
+flex-direction:row;
+justify-content:space-between;
+
+
+`
+
+export const Header = styled.View`
 
 font-family: ${({theme})=> theme.fonts.secondary_600};
-font-size:30px;
-    
+
+
+width:100%;
+height:113px;
+background-color: ${({theme})=>theme.colors.header};
+
+justify-content:flex-end;
+
+padding:32px 24px;
 `;
+
+
+export const TotalCars = styled.Text`
+
+font-size: ${RFValue(15)}px;
+font-family: ${({theme})=> theme.fonts.primary_400};
+color: ${({theme})=>theme.colors.text};
+`
