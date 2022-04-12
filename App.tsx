d0813fd0a8229157738d1661aa44dcc3c054f8ce
Matheus Reis <matheus.reis081@gmail.com>
2022-04-11 21:59:26 -0300

    Configuração inicial feita - tema global e fonts configurados

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/components/Home';
+
+
+import {useFonts,
+Inter_400Regular,
+Inter_500Medium} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading';
+
+import { ThemeProvider } from 'styled-components/native';
+
+
+import {theme} from './src/styles/theme'
+
+
+export default function App() {
+
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Home />
+    </ThemeProvider>
+   
+  )
+ }
